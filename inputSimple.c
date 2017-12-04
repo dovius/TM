230118b5b5f5eb45d2943f9fd44f17e33afb487e
dovius <sidharta014>
2017-12-04 10:32:14 +0200

    bugs found

--- inputSimple.c
+++ inputSimple.c
@@ -1,14 +1,35 @@
 int sum(int a, int b) {
     return a + b;
 }
+int sum1() {
+int a = 0 ; int b = 6;
+    return a + b;
+}
+int sum2(int a) {
+int b = 6;
+    return a + b;
+}
+int sum3(int a, int b) {
+    return a + b;
+}
+string sum4(int a, int b) {
+    return a + b;
+}
 
 int main() {
+    int i = 9;
+    string s = 'a';
+
+    string a = sum4(i, sum1());
+
     for(int i=0;i<=2;i++){
         write 1;
     }
+    string zodis = 'labas';
     for(int i=0;i<2;++i){
         write 2;
     }
     int c = sum(5, 7);
+    write zodis;
     return c;
 }

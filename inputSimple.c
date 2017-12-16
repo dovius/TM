ddaa55c524c4bdabd6f9bfc182164b12ac3eaecf
dovius <sidharta014>
2017-12-16 12:56:07 +0200

    fixes added

--- inputSimple.c
+++ inputSimple.c
@@ -1,3 +1,20 @@
+string sum4(int a, int b) {
+    string c;
+    return c + c;
+}
+
+string sum1(int a) {
+    return 'labas';
+}
+int sum3(int a) {
+a = a + 12;
+    return 12 + 9;
+}
+
+string sum(int a, string zodis) {
+    return zodis;
+}
+
 int darbasSuMayvu(int mas[]) {
     return mas[9];
 }
@@ -13,7 +30,7 @@ int main() {
 
     write zodis;
 
-    int a = darbasSuMayvu(ar);
+    int a = darbasSuMayvu(ar)
     string sudurtinis = zodis + zodis;
     string stringSuInt = zodis + toString(1);
     return 1;

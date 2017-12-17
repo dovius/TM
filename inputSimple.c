9ed5c2ff7ff0a7c856de9bab7a0a872aed8538e1
Čiulada Mindaugas <mindaugas.ciulada@mif.stud.vu.lt>
2017-12-18 00:45:53 +0200

    Error string tipo masyvo elementui priskirta reiksme ir bandyta atspausdinti

--- inputSimple.c
+++ inputSimple.c
@@ -1,6 +1,26 @@
+int sum(int a){
+return a+3
+}
+int testukas(int array[],int b){
+    array[1]=1;
+    array[2]=1+array[1];
+
+return array[2];
+}
+
 int main(){
-string array[20];
-array[2]='labas';
-write array[2];
+//*************************
+  string array[20];
+  array[0]='labas';
+  write array[2];
+//*************************
+//  int array2[30];
+//  array2[2]=2;
+//  int b=sum(array2[2]);
+//  write b;
+//*************************
+// int array3[30];
+//int c=testukas(array3,3); //IndexOutOfBoundsException
+
     return 3;
 }
